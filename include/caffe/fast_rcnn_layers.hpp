2bcdccae03613d9036634754dd9f1e59e3dbd934
Linjie Xing <csljxing@foxmail.com>
2017-07-27 20:57:21 +0800

    Add utility functions to save/load floats onto HDF5

--- include/caffe/fast_rcnn_layers.hpp
+++ include/caffe/fast_rcnn_layers.hpp
@@ -1,3 +1,4 @@
+
 // ------------------------------------------------------------------
 // Fast R-CNN
 // Copyright (c) 2015 Microsoft
@@ -86,7 +87,6 @@ class SmoothL1LossLayer : public LossLayer<Dtype> {
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
@@ -97,6 +97,42 @@ class SmoothL1LossLayer : public LossLayer<Dtype> {
   Blob<Dtype> ones_;
   bool has_weights_;
   Dtype sigma2_;
+};
+
+template <typename Dtype>
+class ProposalLayer : public Layer<Dtype> {
+ public:
+  explicit ProposalLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+    //LOG(FATAL) << "Reshaping happens during the call to forward.";
+  }
+
+  virtual inline const char* type() const { return "ProposalLayer"; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
+
+  int base_size_;
+  int feat_stride_;
+  int pre_nms_topn_;
+  int post_nms_topn_;
+  Dtype nms_thresh_;
+  int min_size_;
+  Blob<Dtype> anchors_;
+  Blob<Dtype> proposals_;
+  Blob<int> roi_indices_;
+  Blob<int> nms_mask_;
 };
 
 }  // namespace caffe
